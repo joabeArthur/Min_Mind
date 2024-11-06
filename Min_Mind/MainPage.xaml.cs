01f4375cc1cd25f2519dbd6f08d13d4f3713b713
joabe <joalokk@gmail.com>
2024-11-06 09:54:02 -0300

    carambolas

--- Min_Mind/MainPage.xaml.cs
+++ Min_Mind/MainPage.xaml.cs
@@ -2,12 +2,80 @@
 
 public partial class MainPage : ContentPage
 {
+	bool Morto = false;
+	bool Pulando = false;
+	const int TempoEntreFrames = 25;
+	int Velocidade = 0;
+	int Velocidade01 = 0;
+	int Velocidade02 = 0;
+	int Velocidade03 = 0;
+	int LarguraJanela = 0;
+	int AlturaJanela = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		CorrigeTamanhoCenario(w, h);
+		CalculaVelocidade(w);
+	}
+
+	void CalculaVelocidade(double w)
+	{
+		Velocidade = (int) (w * 0.01);
+		Velocidade01 = (int) (w * 0.01);
+		Velocidade02 = (int) (w * 0.004);
+		Velocidade03 = (int) (w * 0.008);
+	}
+
+	void CorrigeTamanhoCenario(double w, double h)
+	{
+		foreach (var cor in Fundo_ceu.Children)
+		(cor as Image).WidthRequest = w;
+		foreach (var na in SoleLua.Children)
+		(na as Image).WidthRequest = w;
+		foreach (var o in Arvore.Children)
+		(o as Image).WidthRequest = w;
+		foreach (var docaralh in Chao.Children)
+		(docaralh as Image).WidthRequest = w;
+
+		Fundo_ceu.WidthRequest = w * 1.5;
+		SoleLua.WidthRequest = w * 1.5;
+		Arvore.WidthRequest = w * 1.5;
+		Chao.WidthRequest = w * 1.5;
+	}
 	
+	void GerenciaCanos()
+	{
+		MoveCenario();
+		GerenciaCano(Fundo_ceu);
+		GerenciaCano(SoleLua);
+		GerenciaCano(Arvore);
+		GerenciaCano(Chao);
+	}
+
+	void MoveCenario()
+	{
+		Fundo_ceu.TranslationX -= Velocidade01;
+		SoleLua.TranslationX -= Velocidade02;
+		Arvore.TranslationX -= Velocidade03;
+		Chao.TranslationX -= Velocidade;
+	}
+
+	void GerenciaCenario(HorizontalStackLayout hs1)
+	{
+		var view = (hs1.Children.First() as Image);
+
+		if (view.WidthRequest + hs1.TranslationX < 0)
+		{
+			hs1.Children.Remove(view);
+			hs1.Children.Add(hs1);
+			hs1.TranslationX = view.TranslationX;
+		}
+	}
 }
 
