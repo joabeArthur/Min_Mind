82469b958eab68d1ca1fc328edbc16d465623bca
joabe <joalokk@gmail.com>
2024-11-08 09:06:12 -0300

    caracoles

---
+++ Min_Mind/Animacao.cs
@@ -0,0 +1,70 @@
+namespace Min_Mind;
+
+public  class Animacao
+{
+    protected List<String> Animacao1 = new List<String>();
+    protected List<String> Animacao2 = new List<String>();
+    protected List<String> Animacao3 = new List<String>();
+    protected bool Loop = true;
+    protected int AnimacaoAtiva = 1;
+    bool Parado = true;
+    int FrameAtual = 1;
+    protected Image CompImagem;
+
+    public Animacao(Image ugauga)
+    {
+        CompImagem = ugauga;
+    }
+
+    public void Stop()
+    {
+        Parado = true;
+    }
+
+    public void Play()
+    {
+        Parado = false;
+    }
+
+    public void SetAnimacaoAtiva(int ugauga)
+    {
+        AnimacaoAtiva = ugauga;
+    }
+
+    public void Desenha()
+    {
+        if (Parado)
+        {
+            return;
+        }
+        string NomeArquivo = "";
+        int TamanhoAnimacao = 0;
+
+        if (AnimacaoAtiva == 1)
+        {
+            NomeArquivo = Animacao1[FrameAtual];
+            TamanhoAnimacao = Animacao1.Count;
+        }
+        else if (AnimacaoAtiva == 2)
+        {
+            NomeArquivo = Animacao2[FrameAtual];
+            TamanhoAnimacao = Animacao2.Count;
+        }
+        else if (AnimacaoAtiva == 3)
+        {
+            NomeArquivo = Animacao3[FrameAtual];
+            TamanhoAnimacao = Animacao3.Count;
+        }
+
+        CompImagem.Source = ImageSource.FromFile(NomeArquivo);
+        FrameAtual++;
+
+        if (FrameAtual >= TamanhoAnimacao)
+        {
+            if (Loop)
+            {
+                FrameAtual = 0;
+            }
+        }
+    }
+}
